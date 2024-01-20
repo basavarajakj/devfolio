29571458318a43425d4181b0019266d45c82e637
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-01-20 17:16:22 +0530

    Typography from figma design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
     <main>
-      <h1>Devfolio</h1>
+      <p className="display-large">Typography</p>
+      <p className="display-medium">Typography</p>
+      <p className="display-small">Typography</p>
+      <p className="headline-large">Typography</p>
+      <p className="headline-medium">Typography</p>
+      <p className="headline-small">Typography</p>
+      <p className="title-large">Typography</p>
+      <p className="title-medium">Typography</p>
+      <p className="title-small">Typography</p>
+      <p className="body-large">Typography</p>
+      <p className="body-medium">Typography</p>
+      <p className="body-small">Typography</p>
+      <p className="label-large">Typography</p>
+      <p className="label-medium">Typography</p>
+      <p className="label-small">Typography</p>
     </main>
   );
 }
